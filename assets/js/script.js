82da963019ff0d732e8f69281fc258c51a407cc7
natswatch <nathaliec@me.com>
2020-12-01 22:52:14 -0800

    image loader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 $(document).ready(function() {
   function createEl(htmlString, attrs, ...children) {
     if (typeof htmlString !== "string") {
@@ -152,82 +153,26 @@ $(document).ready(function() {
         ...createCards(events.slice(6,9))
     )
     )
+=======
+require("bootstrap");
+const img1 = require("../../assets/img/food-table.jpg");
+const img2 = require("../../assets/img/grill.jpg");
+>>>>>>> Stashed changes
 
-    pageEl.appendChild(containerEl1);
-    pageEl.appendChild(containerEl2);
-    pageEl.appendChild(containerEl3);
-  }
-
-  if (window.location.href.indexOf("event") > -1) {
-    const currentEvent = JSON.parse(localStorage.getItem("currentEvent")) || {
-        title: "Title Placeholder",
-        subtitle: "",
-        description: ""
-    };
-
-    const pageEl = document.querySelector("#page");
-    
-    const containerEl = createEl("div", {class: "container"},
-      createEl("div", {class: "card mb-3"}, 
-        createEl("img", {class: "card-img-top", style: "width: 5px", src: currentEvent.image || "https://via.placeholder.com/350x150"}),
-        createEl("div", {class: "card-body"}, 
-          createEl("h1", {class: "card-title"}, currentEvent.title || ""),
-          createEl("h2", {class: "text-muted"}, currentEvent.subtitle || ""),
-          createEl("p", {class: "card-text mt-3"}, currentEvent.description || createLoremIpsum(100)),
-          createEl("a", {class: "btn btn-primary", href: "tickets.html"}, "Buy Tickets")
-        )
-      ),
-      
-    )
-    pageEl.appendChild(containerEl)
-  }
-
-  dateConverter = function(UNIX_timestamp) {
-    const a = new Date(UNIX_timestamp);
-    const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    const year = a.getFullYear();
-    const month = months[a.getMonth()];
-    const date = a.getDate();
-    const result =  month + ' ' + date + ', ' + year 
-    return result;
-  };
-
-  if (window.location.href.indexOf("tickets") > -1) {
-
-    const purchaseBtn = document.getElementById("purchaseBtn");
-    const purchaseEmail = document.getElementById("purchaseEmail");
-    const modalEl = document.querySelector(".modal-content");
-    const modalBodyEl = document.querySelector(".modal-body");
-    const modalFooterEl = document.querySelector(".modal-footer");
-
-
-    function purchaseTicket () {
-
-      modalEl.removeChild(modalBodyEl)
-      modalEl.removeChild(modalFooterEl)
-
-      modalEl.append(createEl("div", {class: "modal-body"},
-        createEl("h5", {class: "modal-title"}, 
-        `Thanks for requesting a ticket purchase! We will send an email to ${purchaseEmail.value} to complete the order form!`
-        ),
-      ))
-      
-    }
-    purchaseBtn.addEventListener("click", purchaseTicket);
-  }
+$(document).ready(function() {
   // First image is hard coded in index.html
   const carouselSlides = [
     {
       title: "We travel all over the US",
       subtitle: "Check out our schedule!",
-      img: "./assets/img/food-table.jpg",
+      img: img1,
       btnText: "View Schedule",
       btnUrl: "schedule.html"
     },
     {
       title: "Our food is seriously the bomb!",
       subtitle: "What are you waiting for?",
-      img: "./assets/img/grill.jpg",
+      img: img2,
       btnText: "Purchase Tickets",
       btnUrl: "tickets.html"
     },
